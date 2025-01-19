670392e3ff47a07bd751d915c2bb135774d4ecf4
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-01-19 18:46:09 -0500

    fix: typescript

--- typescript/build.mjs
+++ typescript/build.mjs
@@ -12,16 +12,21 @@ const __dirname = path.dirname(__filename);
  */
 
 /** @type {GenerateApiParams["hooks"]["onFormatRouteName"]} */
-const onFormatRouteName = ({ route }) => route
-    .split("/")
-    .filter(v => !!v) // remove empty splits
-    .map(part => part.charAt(0).toUpperCase() + part.slice(1)) // Capitalize the first letter of each part
+const onFormatRouteName = ({route}) => {
+    const parts = route
+        .split("/")
+        .filter(v => !!v) // remove empty splits
+
+    if(parts.length === 1) return "Get"
+    else return parts.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join("") // Capitalize the first letter of each part
+}
 
 /** @type {GenerateApiParams} */
 await generateApi({
     name: "index.ts",
     output: path.resolve(__dirname),
     input: path.resolve(__dirname, "../OpenAPI.yaml"),
+    apiClassName: "MobileVersionMicroservice",
     httpClientType: "fetch",
     hooks: {
         onFormatRouteName
