1ea522f0f3a10000f9b3a465905e2930d0875e19
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-01-19 18:20:57 -0500

    fix: variables

--- cfg/identifiers.go
+++ cfg/identifiers.go
@@ -1,6 +1,24 @@
 package cfg
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
-var AppleBundleId = os.Getenv("APPLE_BUNDLE_ID")
-var GoogleAppId = os.Getenv("GOOGLE_APP_ID")
+var (
+	AppleBundleId string
+	GoogleAppId   string
+)
+
+func init() {
+	AppleBundleId = os.Getenv("APPLE_BUNDLE_ID")
+	GoogleAppId = os.Getenv("GOOGLE_APP_ID")
+
+	if AppleBundleId == "" {
+		log.Fatal("APPLE_BUNDLE_ID runtime variable not set")
+	}
+
+	if GoogleAppId == "" {
+		log.Fatal("GOOGLE_APP_ID runtime variable not set")
+	}
+}
