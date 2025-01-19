4b99fbb56e13e96ad94cfe3d2f555a1bd1a369f5
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-01-19 16:34:02 -0500

    fix: typescript

--- typescript/tsup.config.ts
+++ typescript/tsup.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from "tsup";
 export default defineConfig({
     entry: ["./index.ts"],
     format: ["cjs", "esm"],
-    outDir: ".",
     dts: true,
     splitting: true,
     sourcemap: true,
